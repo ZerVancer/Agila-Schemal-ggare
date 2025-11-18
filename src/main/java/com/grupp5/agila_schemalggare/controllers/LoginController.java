6eb2b77dd13c4087299880a4c848aeb4217a50a8
JoelSRJE <JoelSRJE@hotmail.com>
2025-11-18 14:05:52 +0100

    View & Controller done

---
+++ src/main/java/com/grupp5/agila_schemalggare/controllers/LoginController.java
@@ -0,0 +1,69 @@
+package com.grupp5.agila_schemalggare.controllers;
+
+import com.grupp5.agila_schemalggare.models.Account;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+
+public class LoginController {
+
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Label statusLabel;
+
+    @FXML
+    private void handleLogin() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.isEmpty()) {
+            changeStatus("Username is required!", "red");
+            return;
+        }
+
+        if (password.isEmpty()) {
+            changeStatus("Password is required!", "red");
+            return;
+        }
+
+        // Måste kolla så att kontot faktiskt finns sparat i en arraylist senare?
+        // Måste uppdateras efter register fungerar, just nu skapas ett automatiskt konto baserat på input i view
+        Account account = new Account(username, password);
+
+        if (account == null) {
+            changeStatus("Username not found!" , "red");
+            return;
+        }
+
+        if (!account.getPassword().equals(password)) {
+            changeStatus("Incorrect password!" , "red");
+            return;
+        }
+
+        changeStatus("Login successful!", "green");
+
+        // Timer för att kalla på en funktion som senare ska rendera kalendern efter lyckad inloggning.
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), event -> clearFields()));
+        timeline.setCycleCount(1);
+        timeline.play();
+    }
+
+    // För framtida användning om användaren vill byta vy från login menyn för att registrera ett konto
+//    @FXML
+//    private void switchToRegisterView() {}
+
+    private void changeStatus(String message, String color) {
+        statusLabel.setText(message);
+        statusLabel.setStyle("-fx-text-fill: " + color);
+        statusLabel.setVisible(true);
+    }
+
+    private void clearFields() {
+        usernameField.clear();
+        passwordField.clear();
+    }
+}
