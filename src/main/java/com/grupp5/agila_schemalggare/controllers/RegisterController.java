266301ab6679c68b42330aa5be500368ea2a4d5d
Elina Samuelsson <108744021+elinasamuelsson@users.noreply.github.com>
2025-11-22 21:21:50 +0100

    Register changes
    Moved register user logic from RegisterController to AccountService

--- src/main/java/com/grupp5/agila_schemalggare/controllers/RegisterController.java
+++ src/main/java/com/grupp5/agila_schemalggare/controllers/RegisterController.java
@@ -1,33 +1,29 @@
 package com.grupp5.agila_schemalggare.controllers;
 
-import com.grupp5.agila_schemalggare.models.Account;
-import com.grupp5.agila_schemalggare.models.User;
+import com.grupp5.agila_schemalggare.services.AccountService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.InputMismatchException;
 
 public class RegisterController {
-    List<Account> accounts = new ArrayList<>(Arrays.asList(
-            new User("username", "?password"),
-            new User("milo_söder", "smörgåsrån4l!fe"),
-            new User("hundenPollux", "p?nnarÄrBäst")
-    ));
+    private AccountService accountService;
+
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @FXML
     TextField usernameInputField;
-
     @FXML
     PasswordField passwordInputField;
-
     @FXML
     Button submitButton;
-
     @FXML
     Label submitConfirmation;
 
@@ -36,55 +32,26 @@ public class RegisterController {
         String username =  usernameInputField.getText();
         String password = passwordInputField.getText();
 
+        boolean creationSuccess;
 
-            if (!validateUsername(username)) {
-                submitConfirmation.setText("Invalid username!\n" +
-                        "Username cannot contain special characters or blank spaces,\n" +
-                        "and be between 6 and 15 characters long.");
-                usernameInputField.clear();
-                passwordInputField.clear();
-                return;
-            }
+        try {
+            creationSuccess = accountService.registerNewAccount(username, password);
+        } catch (Exception exception) {
+            submitConfirmation.setTextFill(Color.RED);
+            submitConfirmation.setText(exception.getMessage());
 
-            if (!validatePassword(password)) {
-                submitConfirmation.setText("Invalid password!\n" +
-                        "Password must contain at least one special character,\n" +
-                        "and be between 6 and 30 characters long.");
-                usernameInputField.clear();
-                passwordInputField.clear();
-                return;
-            }
-
-        Account account = new User(username, password);
-        accounts.add(account);
-
-        //saveUserToFile(); <-- empty method for implementation later
-
-        StringBuilder testMessage = new StringBuilder("Registration successful!");
-
-        for (Account a : accounts) {
-            testMessage.append("\n").append(a.toString());
+            usernameInputField.clear();
+            passwordInputField.clear();
+            return;
         }
 
-        submitConfirmation.setText(testMessage.toString());
-        //submitConfirmation.setText("Registration successful!");
+        if (creationSuccess) {
+            submitConfirmation.setTextFill(Color.GREEN);
+            submitConfirmation.setText("Account creation success!");
+        }
 
         //changeSceneToLogin(); <-- empty method for implementation later
     }
-
-    private boolean validateUsername(String username) { //later also validate that no two users can have the same username
-        return username.length() > 5 && //longer than 5 characters
-                username.length() < 16 && //shorter than 16 characters
-                username.matches("[A-Za-z0-9_.-]+"); //allows letters, numbers, -, _, and .
-    }
-
-    private boolean validatePassword(String password) {
-        return password.length() > 5 && //longer than 5 characters
-                password.length() < 31 && //shorter than 31
-                password.matches(".*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/].*"); //must contain at least one special character
-    }
-
-    private void saveUserToFile(User user) {}
 
     private void changeSceneToLogin() {}
 }
--- src/main/java/com/grupp5/agila_schemalggare/services/AccountService.java
+++ src/main/java/com/grupp5/agila_schemalggare/services/AccountService.java
@@ -1,9 +1,11 @@
 package com.grupp5.agila_schemalggare.services;
 
 import com.grupp5.agila_schemalggare.models.Account;
+import com.grupp5.agila_schemalggare.models.Admin;
 import com.grupp5.agila_schemalggare.models.User;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class AccountService {
@@ -41,8 +43,48 @@ public class AccountService {
         return account;
     }
 
-    // Metod för register.
-//    public Account registerNewAccount() {
-//
-//    }
+    public boolean registerNewAccount(String username, String password) {
+        if (!validateRegisteredUsername(username)) {
+            throw new IllegalArgumentException("Invalid username!\n" +
+                    "Username cannot contain special characters or blank spaces,\n" +
+                    "and be between 6 and 15 characters long.");
+        }
+
+        if (!validateRegisteredUserPassword(password)) {
+            throw new IllegalArgumentException("Invalid password!\n" +
+                    "Password must contain at least one special character,\n" +
+                    "and be between 6 and 30 characters long.");
+        }
+
+        Account account;
+
+        if (registeredUsers.isEmpty()) {
+            account = new Admin(username, password);
+        } else {
+            account = new User(username, password);
+        }
+
+        registeredUsers.add(account);
+        //saveAccountToFile(); <-- empty method for later
+
+        for (Account a : registeredUsers) {
+            System.out.println(a.toString());
+        }
+
+        return true;
+    }
+
+    private boolean validateRegisteredUsername(String username) { //later also validate that no two users can have the same username
+        return username.length() > 5 && //longer than 5 characters
+                username.length() < 16 && //shorter than 16 characters
+                username.matches("[A-Za-z0-9_.-]+"); //allows letters, numbers, -, _, and .
+    }
+
+    private boolean validateRegisteredUserPassword(String password) {
+        return password.length() > 5 && //longer than 5 characters
+                password.length() < 31 && //shorter than 31
+                password.matches(".*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/].*"); //must contain at least one special character
+    }
+
+    //private void saveAccountToFile() {}
 }
