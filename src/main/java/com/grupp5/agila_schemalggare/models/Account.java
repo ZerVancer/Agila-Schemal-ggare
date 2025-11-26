943a035b9bd8259569477eabed3f77033ce9f9ed
JoelSRJE <JoelSRJE@hotmail.com>
2025-11-26 08:32:37 +0100

    Minor changes, somethings needed to start project

--- src/main/java/com/grupp5/agila_schemalggare/models/Account.java
+++ src/main/java/com/grupp5/agila_schemalggare/models/Account.java
@@ -1,5 +1,6 @@
 package com.grupp5.agila_schemalggare.models;
 
+import java.util.List;
 import java.util.UUID;
 
 // Simpel Account klass som tar emot username & password som input.
@@ -8,6 +9,8 @@ public abstract class Account {
     private UUID id;
     private String username;
     private String password;
+    // La till bara för att kunna starta projektet - Joel
+    private List<Event> events;
     //private Calendar calendar; - Placeholder för tillfället.
 
     //Constructors
@@ -33,14 +36,20 @@ public abstract class Account {
         return username;
     }
 
-    // Möjligtvis värt att ta bort senare?
     public String getPassword() {
         return password;
     }
 
     public abstract String getRole();
 
-    // Någon eventuell funktion för om användaren är Admin? Inte nödvändig just nu.
+    // La till bara för att kunna starta projektet - Joel
+    public String addEvent(Event event) {
+        events.add(event);
+
+        return "| Event added: " + event + " |";
+    }
+
+    // Någon eventuell funktion för om användaren är Admin?
     public boolean canEditOthers() {
         return false;
     }
--- src/main/java/com/grupp5/agila_schemalggare/services/AccountService.java
+++ src/main/java/com/grupp5/agila_schemalggare/services/AccountService.java
@@ -5,7 +5,6 @@ import com.grupp5.agila_schemalggare.models.Admin;
 import com.grupp5.agila_schemalggare.models.User;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 
 public class AccountService {
