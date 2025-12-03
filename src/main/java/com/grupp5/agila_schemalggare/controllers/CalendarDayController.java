3216b2c2463ec257526c7a60b2ddf4a3fb948aeb
elinasamuelsson <elina.samuelsson@live.com>
2025-12-03 21:13:09 +0100

    Formatting changes
    Minor formatting of day view to make it more readable and tidy

--- src/main/java/com/grupp5/agila_schemalggare/controllers/CalendarDayController.java
+++ src/main/java/com/grupp5/agila_schemalggare/controllers/CalendarDayController.java
@@ -15,6 +15,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class CalendarDayController {
@@ -30,7 +31,8 @@ public class CalendarDayController {
 
     public void setDate(LocalDateTime date) {
         this.selectedDate = date;
-        dayAndDateLabel.setText(date.toLocalDate().toString());
+      dayAndDateLabel.setText(date.toLocalDate().toString());
+        dayAndDateLabel.setStyle("-fx-font-size: 20; -fx-font-weight: bold");
         loadEventsForDate();
     }
 
@@ -63,14 +65,19 @@ public class CalendarDayController {
         titleRow.getChildren().addAll(titleLabel , editButton, deleteButton );
 
         // === Time row ===
-        HBox timeRow = new HBox(10);
+        HBox timeRow = new HBox(5);
         timeRow.setAlignment(Pos.TOP_CENTER);
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
         LocalDateTime startTime = event.getStartDate();
         LocalDateTime endTime = event.getEndDate();
 
-        Label startTimeLabel = new Label(startTime.getHour() + ":" + startTime.getMinute());
-        Label endTimeLabel = new Label(endTime.getHour() + ":" + endTime.getMinute());
+        String startTimeFormatted = startTime.format(timeFormatter);
+        String endTimeFormatted = endTime.format(timeFormatter);
+
+        Label startTimeLabel = new Label(startTimeFormatted + " - ");
+        Label endTimeLabel = new Label(endTimeFormatted);
 
         timeRow.getChildren().addAll(startTimeLabel, endTimeLabel);
 
